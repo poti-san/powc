737d51c3961e45b5ddf9e0c88bdd6fdf54e8f69c
poti-san <146101605+poti-san@users.noreply.github.com>
2025-04-13 21:29:49 +0900

    Update

--- sphnix apidoc.ps1
+++ sphnix apidoc.ps1
@@ -1,1 +1,1 @@
-sphinx-apidoc -f -o docs\apiref src --no-toc --remove-old
+sphinx-apidoc -f -o docs\source\apiref src --no-toc --remove-old
--- sphnix make html.ps1
+++ sphnix make html.ps1
@@ -1,2 +1,6 @@
-docs\make.bat html
+sphnix\make.bat html
+if (Test-Path html -PathType Container) {
+	Remove-Item docs
+}
+Copy-Item -Recurse sphnix/build/html docs
 pause
