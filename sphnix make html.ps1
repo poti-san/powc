6f5bafc934eb20864d8ac83b26b56b147d0362cb
poti-san <146101605+poti-san@users.noreply.github.com>
2025-04-13 21:46:52 +0900

    Update

--- sphnix make html.ps1
+++ sphnix make html.ps1
@@ -1,6 +1,6 @@
 sphnix\make.bat html
-if (Test-Path html -PathType Container) {
-	Remove-Item docs
+if (Test-Path docs -PathType Container) {
+	Remove-Item -Force -Recurse docs
 }
 Copy-Item -Recurse sphnix/build/html docs
 pause
