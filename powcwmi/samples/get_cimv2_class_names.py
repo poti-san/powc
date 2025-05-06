9462dcfedaea5d94102c4815e892835bec3781df
poti-san <146101605+poti-san@users.noreply.github.com>
2025-05-06 20:22:26 +0900

    Update

--- powcwmi/samples/get_cimv2_class_names.py
+++
@@ -1,9 +0,0 @@
-from operator import attrgetter
-
-from powcwmi import WBEMLocator
-
-locator = WBEMLocator.create()
-server = locator.connect_server(r"root\cimv2")
-
-for cls in sorted(server.create_classenum(None, shallow=False), key=attrgetter("classname")):
-    print(cls.classname)
--- powcwmi/samples/get_pcinfo.py
+++
@@ -1,9 +0,0 @@
-from pprint import pp
-
-from powcwmi import WBEMLocator
-
-locator = WBEMLocator.create()
-service = locator.connect_server(r"root\cimv2")
-
-for instance in service.create_instanceenum("Win32_ComputerSystem"):
-    pp((instance.get("SystemFamily").value.to_str(), instance.get("SystemSKUNumber").value.to_str()))
